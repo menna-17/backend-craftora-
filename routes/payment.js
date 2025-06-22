897634f7f0d3eb04c7c5691ca403fb72a969dd7d
haneenimam <hanen.203100228@compit.aun.edu.eg>
2025-06-22 23:35:11 +0300

    finalll4

--- routes/payment.js
+++ routes/payment.js
@@ -24,8 +24,6 @@ router.post('/create-checkout-session', async (req, res) => {
       payment_method_types: ['card'],
       line_items,
       mode: 'payment',
-      /*       success_url: 'http://localhost:5173/',
-            cancel_url: 'http://localhost:5173/', */
       success_url: `${domainURL}/`,
       cancel_url: `${domainURL}/`,
     });
