8098a9dafeb371c52c66773300a28c73e1206210
haneenimam <hanen.203100228@compit.aun.edu.eg>
2025-06-23 10:53:29 +0300

    finallll

--- routes/admin.js
+++ routes/admin.js
@@ -38,4 +38,17 @@ router.get('/users', auth, roleCheck(['Admin']), async (req, res) => {
   }
 });
 
+// Delete user (Admin only)
+router.delete('/users/:id', auth, roleCheck(['Admin']), async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+    if (!user) return res.status(404).json({ error: 'User not found' });
+
+    res.json({ message: 'User deleted successfully' });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+
 module.exports = router;
